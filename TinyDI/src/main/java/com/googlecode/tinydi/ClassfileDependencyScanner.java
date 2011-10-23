4a170ed1c5d64c6b30b48be9552432f3aaab4cf1
palricsi <palricsi@gmail.com>
2011-10-23 13:47:20 +0000

    recursive classpath scanning including jar files

--- TinyDI/src/main/java/com/googlecode/tinydi/ClassfileDependencyScanner.java
+++ TinyDI/src/main/java/com/googlecode/tinydi/ClassfileDependencyScanner.java
@@ -1,19 +1,13 @@
 package com.googlecode.tinydi;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.*;
 
 /** 
  * Scans the java packages for annotated, injectable classes, and registers those
@@ -28,7 +22,8 @@ public class ClassfileDependencyScanner {
   protected Logger logger = LoggerFactory.getLogger(getClass());
   
   private DependencyRepository repository = DependencyRepository.getInstance();
-  
+  private ClasspathScanner classpathScanner = new ClasspathScanner();
+
   @SuppressWarnings("unchecked")
   public void scan(String  packageRoot) {
     long time = System.currentTimeMillis();
@@ -116,54 +111,22 @@ public class ClassfileDependencyScanner {
   @SuppressWarnings("unchecked")
   private Iterable<Class> getClasses(String packageName)
       throws ClassNotFoundException, IOException {
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    String path = packageName.replace('.', '/');
-    Enumeration<URL> resources = classLoader.getResources(path);
-    List<File> dirs = new ArrayList<File>();
-    while (resources.hasMoreElements()) {
-      URL resource = resources.nextElement();
-      dirs.add(new File(resource.getFile()));
-    }
-    List<Class> classes = new ArrayList<Class>();
-    for (File directory : dirs) {
-      classes.addAll(findClasses(directory, packageName));
-    }
 
-    return classes;
-  }
+    List<String> classNames = classpathScanner.getClassNamesFromPackage(packageName);
 
-  /**
-   * Recursive method used to find all classes in a given directory and subdirs.
-   * 
-   * @param directory
-   *          The base directory
-   * @param packageName
-   *          The package name for classes found inside the base directory
-   * @return The classes
-   * @throws ClassNotFoundException
-   * @see http://stackoverflow.com/questions/862106/how-to-find-annotated-methods-in-a-given-package
-   */
-  @SuppressWarnings("unchecked")
-  private List<Class> findClasses(File directory, String packageName)
-      throws ClassNotFoundException {
     List<Class> classes = new ArrayList<Class>();
-    if (!directory.exists()) {
-      return classes;
-    }
-    File[] files = directory.listFiles();
-    for (File file : files) {
-      if (file.isDirectory()) {
-        classes.addAll(findClasses(file, packageName + "." + file.getName()));
-      } else if (file.getName().endsWith(".class")) {
-        String className = null;
-        try {
-          className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-          classes.add(Class.forName(className));
-        } catch (Throwable t) {
-          logger.warn(MessageFormat.format("Unable to instantiate and therefore scan class {0}.", className ), t);
-        }
+    Set<String> classNameSet = new HashSet<String>();
+
+    for (String cl : classNames) {
+      // filter duplicate class+package names:
+      if (!classNameSet.contains(cl)) {
+        classes.add(Class.forName(cl));
+        classNameSet.add(cl);
       }
     }
+
     return classes;
   }
+
+
 }
